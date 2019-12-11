dcb51c0f9c75dc3492563f073fb12e13926bc48d
eroub <evan.roub@ergonomyx.com>
2019-12-11 15:29:42 -0800

    first work

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0f338af9d96a16462f4a";
+/******/ 	var hotCurrentHash = "754f9b772c9762e4afad";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,71 @@
-const Index = () => (
-    <div>
-        <p>Hello Next.js</p>
-    </div>
+import Layout from '../components/myLayout';
+import Link from 'next/link';
+
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
+  ];
+}
+
+const PostLink = ({ post }) => (
+    <li>
+        <Link href="/p/[id]" as={`/p/${post.id}`}>
+            <a>{post.title}</a>
+        </Link>
+        <style jsx>{`
+            li {
+                list - style: none;
+                margin: 5 px 0;
+            }
+
+            a {
+                text - decoration: none;
+                color: blue;
+                font - family: 'Arial';
+            }
+
+            a: hover {
+                opacity: 0.6;
+            }
+        `}</style>
+    </li>
 );
 
-export default Index;
+export default function Blog() {
+    return(
+        <Layout>
+            <h1>My Blog</h1>
+            <u1>
+                {getPosts().map(post => (
+                    <PostLink key={post.id} post={post} />
+                ))}
+            </u1>
+            <style jsx>{`
+                h1,
+                a {
+                font-family: 'Arial';
+                }
+
+                ul {
+                padding: 0;
+                }
+
+                li {
+                list-style: none;
+                margin: 5px 0;
+                }
+
+                a {
+                text-decoration: none;
+                color: blue;
+                }
+
+                a:hover {
+                opacity: 0.6;
+                }
+            `}</style>
+        </Layout>
+    )
+}
